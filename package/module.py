80a68d7d81e61c001b987d110060013272e9fc4e
keerthana <keerthanah425@gmail.com>
2026-06-18 16:57:01 +0530

    pydandic

---
+++ package/module.py
@@ -0,0 +1,7 @@
+# module.py
+
+def module_func():
+    print("Hello from module_func!")
+def module_func1():
+    print("Hello from module_func!")
+
---
+++ package/module1.py
@@ -0,0 +1,7 @@
+
+def module_func():
+    print("Hello from module_func!")
+
+def module_func1():
+    print("Hello from module_func!")
+
